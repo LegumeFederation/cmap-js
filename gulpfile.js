18072cb63bb3005c85ff6ed9e74e3c959a29cff0
Alex Rice <agr@ncgr.org>
2017-03-06 09:18:06 -0700

    Remove uneeded polyfills and old npm packages.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@
 // https://github.com/gulpjs/gulp/blob/master/docs/recipes/rollup-with-rollup-stream.md
 
 var gulp = require('gulp'),
-    cleanCSS = require('gulp-clean-css'),
     sourcemaps = require('gulp-sourcemaps')
     rename = require('gulp-rename'),
     minify = require('gulp-minify'),
@@ -16,17 +15,6 @@ var gulp = require('gulp'),
     commonjs = require('rollup-plugin-commonjs'),
     replace = require('rollup-plugin-replace'),
     string = require('rollup-plugin-string');
-
-// gulp.task('minify-css', function() {
-//   // minify css and write back into same directory.
-//   // (mithril components will load them from the same dir.)
-//   return gulp.src('./src/ui/css/*.css')
-//     .pipe(cleanCSS())
-//     .pipe(rename(function (path) {
-//       path.extname = ".css-min";
-//     }))
-//     .pipe(gulp.dest("./src/ui/css"));
-// });
 
 gulp.task('default', function() {
     return rollup({
