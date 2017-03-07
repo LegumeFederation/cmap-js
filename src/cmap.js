4d370b0f8fec0af28ba7e28bcca112af48dc9e56
Alex Rice <agr@ncgr.org>
2017-03-07 10:27:13 -0700

    Add pubsub-js (less than 1kb minified and gzipped)
    Publish/Subscribe: https://github.com/mroderick/PubSubJS

--- src/cmap.js
+++ src/cmap.js
@@ -1,5 +1,6 @@
 import { version } from '../package.json';
 import rbush from 'rbush';
+import '../node_modules/pubsub-js/src/pubsub'; // creates global PubSub var
 import { UI } from './ui/ui';
 
 export class cmap {
