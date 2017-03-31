561d4270e26b2987d1445a9368bc6968e45f78e5
Alex Rice <agr@ncgr.org>
2017-03-31 10:37:59 -0600

    Implement HammerJs for mouse and touch gestures (tap, pan, etc).

--- src/ui/layout/LayoutContainer.js
+++ src/ui/layout/LayoutContainer.js
@@ -5,6 +5,7 @@
 import m from 'mithril';
 import Hamster from 'hamsterjs';
 import PubSub from 'pubsub-js';
+import Hammer from 'hammerjs';
 
 import * as layouts from '../../layouts';
 import toolState from '../../state/ToolState';
@@ -27,29 +28,16 @@ export class LayoutContainer {
   /* mitrhril component lifecycle functions */
 
   oninit(vnode) {
-    PubSub.subscribe(layoutMsg, (msg, data) => this.onLayoutChange(msg, data)),
-    PubSub.subscribe(reset, (msg, data) => this.onReset(msg, data))
+    PubSub.subscribe(layoutMsg, (msg, data) => this._onLayoutChange(msg, data)),
+    PubSub.subscribe(reset, (msg, data) => this._onReset(msg, data))
   }
 
   oncreate(vnode) {
     let el = vnode.dom; // this is the outer m('div') from view()
-
-    // TODO: add multitouch event handlers -- w/ hammerjs?
-
-    // pan the layout on mouse drag gestures.
-    // (note: this is different than html5 dragndrop)
-    el.addEventListener('mousedown', evt => this._onMouseDown(evt));
-    el.addEventListener('mousemove', evt => this._onMouseMove(evt));
-    el.addEventListener('mouseup', evt => this._onMouseUp(evt));
-    el.addEventListener('mouseenter', evt => this._onMouseEnter(evt));
-    el.addEventListener('mouseexit', evt => this._onMouseExit(evt));
-
-    Hamster(el).wheel((event, delta, deltaX, deltaY) => {
-      this._onZoom(event, delta, deltaX, deltaY);
-    });
+    this._setupEventHandlers(el);
 
     // use our dom element's width and height as basis for our layout
-    let domRect = vnode.dom.getBoundingClientRect();
+    let domRect = el.getBoundingClientRect();
     console.assert(domRect.width > 0, 'missing dom element width');
     console.assert(domRect.height > 0, 'missing dom element height');
     this._updateBounds(domRect);
@@ -60,76 +48,6 @@ export class LayoutContainer {
     console.assert(domRect.width > 0, 'missing width');
     console.assert(domRect.height > 0, 'missing height');
     this._updateBounds(domRect);
-  }
-
-  /* dom event handlers */
-  _onZoom(evt, delta, deltaX, deltaY) {
-    // convert to a normalized scale factor
-    let normalized = deltaY / this.bounds.height;
-    this.scale += normalized;
-    m.redraw();
-    evt.preventDefault();
-    evt.stopPropagation();
-  }
-
-  _onMouseDown(evt) {
-    this.dragging = true;
-  }
-
-  _onMouseUp(evt) {
-    this.dragging = false;
-  }
-
-  _onMouseMove(evt) {
-    if(this.dragging) {
-      this._applyPan(evt.movementX, evt.movementY);
-      m.redraw();
-    }
-  }
-
-  _onMouseEnter(evt) {
-    this.dragging = false;
-  }
-
-  _onMouseExit(evt) {
-    this.dragging = false;
-  }
-
-  /* pub/sub callback functions */
-  onReset(msg, data) {
-    m.redraw();
-  }
-
-  onLayoutChange(msg, data) {
-    m.redraw();
-  }
-
-  /* internal functions  */
-
-  // here we capture the bounds of the outer dom element from view().
-  // this is leveraging the html/css layouting to fill available space.
-  _updateBounds(domRect) {
-    let newBounds = new Bounds(domRect);
-    let dirty = ! Bounds.equals(this.bounds, newBounds);
-    this.bounds = newBounds;
-    // create an initial bounds for the scrollable/zoomable content-pane
-    if(! this.contentPaneBounds) {
-      this.contentPaneBounds = {
-        top: 0,
-        left: 0,
-        width: this.bounds.width,
-        height: this.bounds.height
-      };
-   }
-    // trigger a redraw so the child components see the new bounds of their
-    // container in the dom.
-    if(dirty) setTimeout(m.redraw);
-  }
-
-  _applyPan(deltaX, deltaY) {
-    this.contentPaneBounds.left += deltaX;
-    this.contentPaneBounds.top += deltaY;
-    m.redraw();
   }
 
   /* mithril component render callback */
@@ -151,5 +69,86 @@ export class LayoutContainer {
         m(this.circosLayout)
       ])
     ]);
+  }
+
+  /* private functions  */
+
+  _setupEventHandlers(el) {
+    // hamsterjs for normalized mouse scrollwheel events
+    Hamster(el).wheel((event, delta, deltaX, deltaY) => {
+      this._onMouseWheel(event, delta, deltaX, deltaY);
+    });
+    // hammers for normalized mouse and touch gestures: zoom, pan, click
+    let hammer = Hammer(el);
+    // enable pan gesture support all directions. warning: this will block the
+    // vertical scrolling on a touch-device while on the element.
+    hammer.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+    hammer.on('panmove', (evt) => this._onPan(evt));
+    hammer.on('pinch', (evt) => this._onZoom(evt, null, evt.deltaX, evt.deltaY));
+  }
+
+  /* dom event handlers */
+  _onMouseWheel(evt, delta, deltaX, deltaY) {
+    // convert to a normalized scale factor
+    let normalized = deltaY / this.bounds.height;
+    this.scale += normalized;
+    m.redraw();
+    evt.preventDefault();
+    evt.stopPropagation();
+  }
+
+  _onPan(evt) {
+    // hammer provides the delta x,y in a distance since the start of the gesture
+    // so need to convert it to delta x,y for this event.
+    let delta = {};
+    if(this.lastPanEvent) {
+      delta.x = -1 * (this.lastPanEvent.deltaX - evt.deltaX);
+      delta.y = -1 * (this.lastPanEvent.deltaY - evt.deltaY);
+    }
+    else {
+      delta.x = evt.deltaX;
+      delta.y = evt.deltaY;
+    }
+    this.contentPaneBounds.left += delta.x;
+    this.contentPaneBounds.top += delta.y;
+    m.redraw();
+    this.lastPanEvent = evt;
+  }
+
+  _onPinch(evt) {
+    // TODO: handle type of event (e.g. pinch move, vs pinch in/out)
+    // this.contentPaneBounds.left += evt.deltaX;
+    // this.contentPaneBounds.top += evt.deltaY;
+    // m.redraw();
+  }
+
+  /* pub/sub callbacks */
+
+  _onReset(msg, data) {
+    m.redraw();
+  }
+
+  _onLayoutChange(msg, data) {
+    m.redraw();
+  }
+
+  // here we capture the bounds of the outer dom element from view().
+  // this is leveraging the html/css layouting to fill available space.
+  _updateBounds(domRect) {
+    let newBounds = new Bounds(domRect);
+    let dirty = ! Bounds.equals(this.bounds, newBounds);
+    this.bounds = newBounds;
+    // create an initial bounds for the scrollable/zoomable content-pane
+    if(! this.contentPaneBounds) {
+      this.contentPaneBounds = {
+        top: 0,
+        left: 0,
+        width: this.bounds.width,
+        height: this.bounds.height
+      };
+   }
+    // trigger a redraw so the child components see the new bounds of their
+    // container in the dom.
+    if(dirty) setTimeout(m.redraw);
   }
 }
