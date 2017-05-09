ef19ad380666d23c6084618f64d4139bb481e44e
Alex Rice <agr@ncgr.org>
2017-05-09 12:11:32 -0600

    Add a polyfill ecma-proposal-math-extensions for some Math. goodness from Es-Next.
    * Adds Math.scale, fscale, clamp, radians, degrees

--- src/polyfill/index.js
+++ src/polyfill/index.js
@@ -1,5 +1,10 @@
 /**
  * A polyfill is a type of shim that retrofits legacy browsers with modern
- * HTML5/CSS3 features.
+ * Javascript, HTML5, or CSS3 features.
  */
+
+ // adds Math.scale, fscale, clamp, radians, degrees
+import 'ecma-proposal-math-extensions';
+
+// adds elementsFromPoint
 import './elementsFromPoint';
