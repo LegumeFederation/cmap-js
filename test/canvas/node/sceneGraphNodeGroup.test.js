e985ef701cee4407d658827410fd2ed496164eb8
Andrew Wilkey <awilkey@iastate.edu>
2017-05-18 11:44:26 -0500

    tests for sceneGraphNodeTrack and Group

---
+++ test/canvas/node/sceneGraphNodeGroup.test.js
@@ -0,0 +1,28 @@
+import {expect} from 'chai';
+import {Bounds} from '../../../src/model/Bounds';
+import {Group} from '../../../src/canvas/node/SceneGraphNodeGroup';
+
+describe('SceneGraphNodeGroup test', function() {
+  it('constructor works', function() {
+    let bounds = new Bounds({
+      top: 1,
+      bottom: 11,
+      left: 10,
+      right: 20,
+      width: 10,
+      height: 10
+    });
+    let parent = {};
+    let params = {
+      parent: parent,
+      bounds: bounds,
+      tags: ['test'],
+      rotation: 45
+    };
+    let node = new Group(params);
+    expect(node.parent).to.equal(parent);
+    expect(node.bounds).eql(bounds);
+    expect(node.tags).eql(['test']);
+    expect(node.rotation).to.equal(45);
+  });
+});
