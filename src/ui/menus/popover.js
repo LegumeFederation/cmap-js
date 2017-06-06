320b4a4b625bed8b12dbf47b27a5d07ed8c8e70e
Andrew Wilkey <awilkey@iastate.edu>
2017-06-06 11:38:22 -0500

    add collapsible information in popovers

--- src/ui/menus/popover.js
+++ src/ui/menus/popover.js
@@ -10,11 +10,6 @@ import {RegisterComponentMixin} from '../RegisterComponentMixin';
 
 export class Popover extends mix(Menu).with(RegisterComponentMixin){
 
-   constructor(){
-    super();
-    this.data = [];
-   }
-
   oninit(vnode){
     super.oninit(vnode);
   }
@@ -25,14 +20,8 @@ export class Popover extends mix(Menu).with(RegisterComponentMixin){
   view(vnode) {
     let b = vnode.attrs.domBounds || {};
 		let info = vnode.attrs.info || {data:[]};
-    console.log(vnode);
 		return m('div', {
-       class: `biomap-info`,
-       //style: `left: ${info.left+b.left}px; top: ${info.top+b.top}px;
-       //        width: 15em; height: 10em; border:1px solid #bbb;
-       //        background: white; visibility: ${info.visible};
-       //        position: absolute; display: inline-block; overflow-y:auto;
-       //        z-index:10000; border-radius:4px;`,
+       class: 'biomap-info',
        style: `left: ${info.left+b.left}px; top: ${info.top+b.top}px;
                visibility: ${info.visible};`,
      },this._generateInner(info.data));
@@ -41,18 +30,29 @@ export class Popover extends mix(Menu).with(RegisterComponentMixin){
   _generateInner(data){
     if(!data) return;
 
-    let bob = data.map(item => {
-      console.log('popitem',item);
+    let popover = data.map(item => {
       let start = m('div', 'start:  '+ item.model.coordinates.start);
       let stop = m('div', 'stop:  '+ item.model.coordinates.stop);
       let tags = item.model.tags.length > 0 && typeof item.model.tags[0] != 'undefined' ? m('div','tags:  ',item.model.tags.join('\n')) : [];
       let aliases = item.model.aliases.length > 0 && typeof item.model.aliases[0] != 'undefined'  ?  m('div','aliases:  ',item.model.aliases.join('\n')) : [];
-      return [m('div',{class:'biomap-info-name'},item.model.name),
-        m('div',{class:'biomap-info-data'},[start,stop,tags, aliases])
+      return [m(this._buttonTest(),{targetId:item.model.name}),
+        m('div',{class:'biomap-info-data', id:`biomap-info-${item.model.name}`, style: 'display: none;'},[start,stop,tags, aliases])
       ];
     });
     
-    return m('div',{},bob);
+    return m('div',{},popover);
+  }
+
+  _buttonTest(){
+    return{
+      view: function(vnode){
+        let targetName = `biomap-info-${vnode.attrs.targetId}`;
+        return  m('div', {class:'biomap-info-name', onclick: function() {
+          let target = document.getElementById(targetName);
+          target.style.display = target.style.display == 'none' ? 'block' : 'none';}},
+          vnode.attrs.targetId);
+      }
+    };
   }
 
 
