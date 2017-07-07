319f3269ba187b6aa41cfe43ec60486352939fca
adf-ncgr <adf@ncgr.org>
2017-07-07 17:52:12 -0600

    fix to bug introduced in merge. the linking service returns a json with "href" specified as the attribute in the returned json not "url"

--- src/ui/menus/Popover.js
+++ src/ui/menus/Popover.js
@@ -42,7 +42,9 @@ export class Popover extends mix(Menu).with(RegisterComponentMixin){
                 l => {return m('div', {}, m('a', {'href' : l.url.replace(/\${item.id}/, item.model.name)}, l.text));}
             ).concat( 
               item.model.source.linkouts.some(l => {return l.isLinkingService && item.model.typeHasLinkouts;}) ? 
-                (item.model.links == undefined ? m('img[src=images/ajax-loader.gif]') : item.model.links.map(l => {return m('div',{}, m('a', {href:l.url}, l.text));})) 
+                (item.model.links == undefined ? m('img[src=images/ajax-loader.gif]') : item.model.links.map(
+                l => {return m('div',{}, m('a', {href:l.href}, l.text));})
+            ) 
                 : [] 
             )
          ) : [];
