a558fcdc90cad8ffe5a744fe4e829ff4730e889d
Andrew Wilkey <awilkey@iastate.edu>
2017-07-13 13:32:38 -0500

    tweak QTL selection modal

--- src/ui/menus/Feature.js
+++ src/ui/menus/Feature.js
@@ -14,6 +14,9 @@ export class FeatureMenu extends mix(Menu).with(RegisterComponentMixin){
 
   oninit(vnode){
     super.oninit(vnode);
+    this.tagList = vnode.attrs.info.parent.parent.model.tags.sort();
+    this.settings = vnode.attrs.info.parent.parent.model.qtlGroups[vnode.attrs.order];
+    this.selected = { name: this.settings.filter, index: this.tagList.indexOf(this.settings.filter)};
   }
    
   /**
@@ -21,30 +24,55 @@ export class FeatureMenu extends mix(Menu).with(RegisterComponentMixin){
    */
   view(vnode) {
     let info = vnode.attrs.info || {};
-    info.selectedId = "none";
     let bounds = vnode.attrs.bounds || {};
     let order = vnode.attrs.order || 0;
     let modal = this;
-    let pV = '';
+    modal.rootNode = vnode;
+  
     return m('div', {
        class: 'feature-menu',
        style: `position:absolute; left: 0px; top: 0px; width:${bounds.width}px;height:${bounds.height}px`,
        onclick: function(){console.log('what',this);}
-     },[this._inner(info,order), m('button',{onclick: function(){ console.log('what what', info.selectedId);
-       PubSub.publish(featureUpdate, null);
-       vnode.dom.remove(vnode);}},'close modal')]);
+     },[this._dropDown(modal), this._applyButton(modal), this._closeButton(modal)]);
+  }
+  
+  _applyButton(modal){
+     return  m('button',{
+        onclick: function(){
+          console.log('what what', modal.settings);
+          modal.settings.filter = modal.selected.name;
+          PubSub.publish(featureUpdate, null);
+          modal.rootNode.dom.remove(modal.rootNode);
+        }
+      },'Apply Selection');
   }
 
+  _closeButton(modal){
+     return  m('button',{
+        onclick: function(){
+          modal.rootNode.dom.remove(modal.rootNode);
+        }
+      },'Close');
+  }
 
-  _inner(data,order){
-    let settings = data.parent.parent.model.qtlGroups[order];
-    let tagList = data.parent.parent.model.tags;
-    this.selectedId = settings.filter;
-    data.selectedId = this.selectedId;
-    this.data = tagList;
-    console.log("info", data,settings,tagList);
-    return m('select',{onchange: function(e){settings.filter = e.target.value;}},[tagList.map(tag => {
-      return m('option',{ value:tag}, tag);
+  _dropDown(modal){
+    console.log('what inner', modal);
+    let selector = this;
+    return m('select',{
+      config: function(selectElement, isinit){
+        if(isinit) { 
+          selector.selectElement = modal.selected.name;
+          selector.update(modal.tagList);
+          return;
+        }
+          selector.selectElement = selectElement;
+          selector.update(modal.tagList);
+      },
+      onchange: function(e){
+        selector.selected.name = e.target.value;
+        selector.selected.index = modal.tagList.indexOf(e.target.value);
+       }},[modal.tagList.map(tag => {
+      return m('option', tag);
       })
     ])
   }
