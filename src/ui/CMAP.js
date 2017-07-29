cd20478a4aae4255dc4db4ba2cbfd99b55662dac
adf-ncgr <adf@ncgr.org>
2017-07-29 12:50:35 -0600

    add config as URL query param, to enable serving up alternate datasets conveniently

--- src/ui/CMAP.js
+++ src/ui/CMAP.js
@@ -10,16 +10,20 @@ import {UI} from './UI';
 /* istanbul ignore next: mithril-query does not work with m.mount, and dom id is hardcoded as well */
 export class CMAP {
 
-  load() {
+  load(configURL) {
     this.rootElement = document.getElementById('cmap-ui');
     this.appState = new AppModel({});
     this.UI = new UI(this.appState);
     m.mount(this.rootElement, this.UI);
 
+    if (configURL === null) {
+      configURL = 'cmap.json';
+    }
+
     this.appState.status = 'loading configuration file...';
     this.appState.busy = true;
 
-    m.request('cmap.json').then( config => {
+    m.request(configURL).then( config => {
       let numSources = config.sources.length;
       let plural = numSources > 1 ? 's': '';
       this.appState.status = `loading ${numSources} data file${plural}...`;
