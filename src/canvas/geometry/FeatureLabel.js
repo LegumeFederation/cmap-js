d763b435e6d26a7f2631df295a78643eba0f38b8
Andrew Wilkey <awilkey@iastate.edu>
2017-08-28 13:04:28 -0500

    fix labels

--- src/canvas/geometry/FeatureLabel.js
+++ src/canvas/geometry/FeatureLabel.js
@@ -25,7 +25,7 @@ export class FeatureLabel extends SceneGraphNodeBase {
   }
 
   draw(ctx) {
-    let y = this._translateScale(this.model.coordinates.start) * this.pixelScaleFactor;
+    let y = (this._translateScale(this.model.coordinates.start)+(this.featureMap.view.base.start*-1)) * this.pixelScaleFactor;
     this.bounds.top = y;
     this.bounds.bottom = y + 12;
     let gb = this.globalBounds || {};
