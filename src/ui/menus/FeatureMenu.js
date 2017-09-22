fe31b7500280e7cef2ae6b3e06812e44067adf02
Andrew Wilkey <awilkey@iastate.edu>
2017-09-22 17:14:18 -0500

    fix populating unpopulated maps

--- src/ui/menus/FeatureMenu.js
+++ src/ui/menus/FeatureMenu.js
@@ -30,13 +30,14 @@ export class FeatureMenu {
 		let trackGroups = [];
     let defaultSettings = model.qtlGroups && model.qtlGroups[order] != undefined ? {filters:model.qtlGroups[order].filters.slice(0),trackColor:model.qtlGroups[order].trackColor.slice(0)} : undefined;
 		if(order == undefined){
-			order = model.qtlGroups.length;
+			order = model.qltGroups ? model.qtlGroups.length : 0;
 		}
 
     if(!model.qtlGroups || model.qtlGroups[0] === undefined){
       order = 0;
       settings = {filters:[tagList[0]],trackColor:['red']};
       trackGroups[0]= settings;
+      model.qtlGroups = [];
     } else {
       trackGroups = model.qtlGroups.slice(0);
       if(!trackGroups[order]){
