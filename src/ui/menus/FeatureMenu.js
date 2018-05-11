045b717dd0c439c37dc9232e141d231d2eaff6d8
awilkey <awilkey@iastate.edu>
2018-05-11 12:57:23 -0500

    Fix accidental duplication of new feature tracks being added.

--- src/ui/menus/FeatureMenu.js
+++ src/ui/menus/FeatureMenu.js
@@ -218,10 +218,12 @@ export let _applyButton = {
   view: function (vnode) {
     return m('button', {
       onclick: function () {
+        console.log('addfeature prePub',vnode.attrs);
         vnode.attrs.config.filters = vnode.attrs.newData.map(data => {return data.name;});
-        vnode.attrs.model.tracks[vnode.attrs.order] = vnode.attrs.config;
+        vnode.attrs.model.tracks[vnode.attrs.order] = JSON.parse(JSON.stringify(vnode.attrs.config));
         PubSub.publish(featureUpdate, {mapIndex: vnode.attrs.bioMapIndex});
         m.redraw();
+        console.log('addfeature postPub',vnode.attrs);
         closeModal();
       }
     }, 'Apply Selection');
