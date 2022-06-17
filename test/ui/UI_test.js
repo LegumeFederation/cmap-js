995d94e2a7d4f2c81259bc268eaadac4e27a423c
Jeremy Wessels <jjw42009@gmail.com>
2022-06-17 10:22:21 -0500

    Fixed failing UI test

--- test/ui/UI_test.js
+++ test/ui/UI_test.js
@@ -1,9 +1,11 @@
 import mq from './mithrilQuerySetup';
 import {UI} from '../../src/ui/UI';
+import {AppModel} from '../../src/model/AppModel';
 
 describe('UI component', function() {
   it('should generate appropriate output', function() {
-    let component = new UI();
+    const model = new AppModel();
+    let component = new UI(model);
     let out = mq(component);
     out.should.have('div.cmap-layout.cmap-vbox');
     out.should.have('div.cmap-layout-viewport.cmap-hbox');
