7e1c1d755d33fa0de9da783557037dbff3ddcae0
Chee Hau Fong <66919882+Cheehau0659@users.noreply.github.com>
2023-09-04 05:39:50 -0500

    fix page not found error for `npm run watch` in a codespace (#131)
     Listen on 127.0.0.1 for codespace compatibility

--- rollup.config.js
+++ rollup.config.js
@@ -54,7 +54,7 @@ export default {
       open: true,
       verbose: true,
       contentBase: '',
-      host: 'localhost',
+      host: '127.0.0.1',
       port: 8888
     })),
     (process.env.WATCH === 'yes' && livereload()),
